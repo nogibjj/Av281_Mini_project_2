d9ca16d6d174f18935ec35cb633c604819ab8063
adlerviton <143657881+adlerviton@users.noreply.github.com>
2023-09-06 20:40:11 +0000

    fixed accidental indent in test_main function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 def PDdescribe(data):
+    '''function which returns descriptive stats about input data'''
     df = pd.DataFrame(data)
     return df.describe()
--- test_main.py
+++ test_main.py
@@ -8,14 +8,14 @@ from main import PDdescribe
 
 def test_desc():
     """Function calling PDdescribe"""
-        data = {'A': [1, 2, 3, 4, 5], 'B': [10, 20, 30, 40, 50]}
-        
-        # Call the function to be tested
-        result = PDdescribe(data)
-        
-        # Define your expected output
-        expected_output = pd.DataFrame({
-            'A': [5.0, 3.0, 1.581139, 1.0, 2.0, 3.0, 4.0, 5.0],
-            'B': [50.0, 30.0, 15.811388, 10.0, 20.0, 30.0, 40.0, 50.0]
-        }, index=['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'])
-        assert result == expected_output
+    data = {'A': [1, 2, 3, 4, 5], 'B': [10, 20, 30, 40, 50]}
+    
+    # Call the function to be tested
+    result = PDdescribe(data)
+    
+    # Define your expected output
+    expected_output = pd.DataFrame({
+        'A': [5.0, 3.0, 1.581139, 1.0, 2.0, 3.0, 4.0, 5.0],
+        'B': [50.0, 30.0, 15.811388, 10.0, 20.0, 30.0, 40.0, 50.0]
+    }, index=['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'])
+    assert result == expected_output

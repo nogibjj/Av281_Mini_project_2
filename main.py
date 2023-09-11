ef8f6b8a273f39af90e97d70a53f4da79c3f8af5
adlerviton <143657881+adlerviton@users.noreply.github.com>
2023-09-11 11:09:29 -0400

    Update main.py to take in csv instead of dataframe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def PDdescribe(data):
+def PDdescribe(filename):
     '''function which returns descriptive stats about input data'''
-    df = pd.DataFrame(data)
+    df = pd.read_csv(filename)
     return df.describe()

24b2b73d57c52331a83e443565dd5202f0775b4c
adlerviton <143657881+adlerviton@users.noreply.github.com>
2023-09-13 21:16:17 +0000

    basic codespace updates, add get-pip.py

--- test_main.py
+++ test_main.py
@@ -11,13 +11,14 @@ import matplotlib.pyplot as plt
 def test_desc():
     """Function calling PDdescribe"""
     # Call the function to be tested
-    result = PDdescribe('nba.csv')
+    result = PDdescribe("nba.csv")
     assert result.shape == (8, 4)
 
-#run data visualization code
-nba = pd.read_csv('nba.csv')
-plt.scatter(nba['Age'], nba['Weight'])
-plt.title('NBA player Weight vs Age')
-plt.xlabel('Height')
-plt.ylabel('Age')
+
+# run data visualization code
+nba = pd.read_csv("nba.csv")
+plt.scatter(nba["Age"], nba["Weight"])
+plt.title("NBA player Weight vs Age")
+plt.xlabel("Height")
+plt.ylabel("Age")
 plt.show()

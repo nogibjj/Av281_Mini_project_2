d5e74645e343921ff7e6055664d998300d3408d3
adlerviton <143657881+adlerviton@users.noreply.github.com>
2023-09-13 15:39:40 -0400

    Update main.py to have data visualization

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def PDdescribe(filename):
     '''function which returns descriptive stats about input data'''
     df = pd.read_csv(filename)
     return df.describe()
+
+print(PDescribe('nba.csv'))
+
+nba = pd.read_csv('nba.csv')
+nba.plot(kind = 'scatter', x = 'Height', y = 'Weight')
+plt.show()

7f5fd00527bbbb30d07b8a59ae2de5d4b4d8aa06
adlerviton <143657881+adlerviton@users.noreply.github.com>
2023-09-13 15:42:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def PDdescribe(filename):
     '''function which returns descriptive stats about input data'''
     df = pd.read_csv(filename)
     return df.describe()
 
-print(PDescribe('nba.csv'))
+results = PDescribe('nba.csv')
+
+print(results)
 
 nba = pd.read_csv('nba.csv')
 nba.plot(kind = 'scatter', x = 'Height', y = 'Weight')
